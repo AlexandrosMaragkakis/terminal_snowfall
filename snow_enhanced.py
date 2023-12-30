f1c155720553727f3d39b37bf5f49f9571b1cdca
Alexandros Maragkakis <alexm280470@gmail.com>
2023-12-30 13:03:36 +0200

    Add ascii house

--- snow_enhanced.py
+++ snow_enhanced.py
@@ -14,10 +14,10 @@ import random
 import time
 
 # Constants
-SNOW_DENSITY = 100 # more snow
-DELAY = 0.2 # faster snow
+SNOW_DENSITY = 2 # more snow
+DELAY = 0.1 # faster snow
 MELT_DELAY = 1 # when to melt
-MELTING_FACTOR = 5000 # how much to melt
+MELTING_FACTOR = 1000 # how much to melt
 snowflakes = ['❄', '❆', '❅', '❃', '❇', '❈', '❉', '❊', '❋']
 
 # Get terminal size
@@ -28,6 +28,42 @@ h = term.lines
 # Initialize empty grid
 grid = [[' '] * w for _ in range(h)]
 
+# Fill bottom with '~'
+for i in range(w):
+    grid[h-1][i] = '¯'
+
+ascii_house = """
+  []___
+ /    /\\
+/____/__\\
+|[][]||||
+"""
+
+ascii_rows = ascii_house.split('\n')
+
+house_height = len(ascii_rows)
+house_width = len(ascii_rows[-1])
+
+# Calculate the starting position to embed the ASCII art in the grid
+ascii_start_row = h - house_height 
+ascii_start_col = 10 + house_width // 2  # Adjusted to be to the right of the center
+
+
+# Embed the ASCII art into the grid
+for i, row in enumerate(ascii_rows):
+    # Find the first non-space character in the row
+    start_index = 0#row.find(row.lstrip())
+    for j, char in enumerate(row):
+        # Skip leading spaces and update the grid
+        if char != ' ':
+            grid[ascii_start_row + i][ascii_start_col + start_index + j] = char
+
+
+
+
+
+
+
 def melt_snowflakes():
     """
     Melt snowflakes in the grid.
@@ -37,7 +73,7 @@ def melt_snowflakes():
     """
     for j in range(w):
         for i in range(h - 1, 0, -1):
-            if grid[i][j] != ' ' and random.random() < SNOW_DENSITY / MELTING_FACTOR:
+            if grid[i][j] in snowflakes and random.random() < SNOW_DENSITY / MELTING_FACTOR:
                 grid[i][j] = ' '
 
 def generate_new_row():
@@ -58,7 +94,7 @@ def update_grid():
     """
     for i in range(h - 1, 0, -1):
         for j in range(w):
-            if grid[i][j] == ' ':
+            if grid[i][j] == ' ' and grid[i-1][j] in snowflakes:
                 grid[i][j] = grid[i-1][j]
                 grid[i-1][j] = ' '
 
